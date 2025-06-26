f5bb160c1895cb9fb708682e462c389a5bcb2989
Cristian <daniel981311@gmail.com>
2025-06-26 16:46:03 -0500

    Primer cambio

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using appComercial.Models;
+
+namespace appComercial.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Producto> productos { get; set; }
+        public DbSet<Categoria> categorias { get; set; }
+        public DbSet<Proveedores> proveedores { get; set; }
+    }
+}
---
+++ Models/Proveedores.cs
@@ -0,0 +1,10 @@
+﻿namespace appComercial.Models
+{
+    public class Proveedores
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+    }
+}
+    
