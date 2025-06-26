2e5a1553b465e65365a57a3e9e41ddb7706e9df1
Cristian <daniel981311@gmail.com>
2025-06-26 16:50:19 -0500

    Update

--- obj/Debug/net8.0/appComercial.AssemblyInfo.cs
+++ obj/Debug/net8.0/appComercial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appComercial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ad91f6ef3b6de3d1953a24b37b9310522ab3ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("appComercial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appComercial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
